e0b5f6a0e2658ecf6237023c67ce97d4ef9e7d30
Johnny <johnny.d.peterson@gmail.com>
2021-10-09 15:05:32 -0400

    init

---
+++ 06-access-the-camera/Starter/QuickCamera/QuickCamera/CameraViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CameraViewController.swift
+//  QuickCamera
+//
+//  Created by Johnny Peterson on 10/9/21.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraViewController: UIViewController {
+    
+    let captureSession = AVCaptureSession()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
